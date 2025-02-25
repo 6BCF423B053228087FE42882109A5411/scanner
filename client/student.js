f8c1d9ef2f100604138cf970e37820e93a32ff29
6BCF423B053228087FE42882109A5411 <144575793+6BCF423B053228087FE42882109A5411@users.noreply.github.com>
2025-02-25 02:07:22 -0800

    Add files via upload

---
+++ client/student.js
@@ -0,0 +1,7 @@
+const students = [
+    { id: "32122U18017", name: "sanjay", class: "III BCA" },
+    { id: "112", name: "Jane Smith", class: "II BSc" },
+    { id: "113", name: "Mike Johnson", class: "I MCA" },
+    { id: "114", name: "Emily Davis", class: "III BSc" },
+    { id: "115", name: "Robert Brown", class: "II BCA" }
+];
